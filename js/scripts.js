0e527d95f807eed4b476d648ef131f0b5d03232a
saimashahh <ssaima@gmail.com>
2024-12-27 16:34:56 -0700

    hamburgermenu and menu toggle ex2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hi! Welcome to My Portfolio")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+  
